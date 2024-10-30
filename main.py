5d358a95bbd49d3385d3d56af801c75c590582de
Juan-Sebastian-Rojas-Sanchez <186082423+Juan-Sebastian-Rojas-Sanchez@users.noreply.github.com>
2024-10-30 10:24:17 -0500

    ejercicio pitagoras

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-#Número invertido
-# Get input from the user
-number = input("Enter number: ")
+#pitagoras
+import math
 
-# Reverse and print the number
-print(number[::-1])
+# Get input from the user
+a = float(input("Enter leg a: "))
+b = float(input("Enter leg b: "))
+
+# Calculate the length of the hypotenuse
+c = math.sqrt(a**2 + b**2)
+
+# Print the result
+print(f"The hypotenuse is {c}.")
