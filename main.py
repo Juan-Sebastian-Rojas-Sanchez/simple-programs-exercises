c24157bcc6f1f2b6e6581f8dbeb5ba6093290df8
Juan-Sebastian-Rojas-Sanchez <186082423+Juan-Sebastian-Rojas-Sanchez@users.noreply.github.com>
2024-10-30 10:21:56 -0500

    ejercicio Conversión de unidades de longitud

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-#promedio
-# Ask the user to enter the grades
-grade1 = float(input("First grade: "))
-grade2 = float(input("Second grade: "))
-grade3 = float(input("Third grade: "))
-grade4 = float(input("Fourth grade: "))
+#Conversión de unidades de longitud
+# Get input from the user
+cm_value = float(input("Enter the value in centimeters: "))
 
-# Calculate the average
-average = (grade1 + grade2 + grade3 + grade4) / 4
+# Convert centimeters to inches
+inches_value = cm_value / 2.54
 
-# Display the result
-print(f"The average is: {average}")
+# Print the result
+print(f"{cm_value} cm is equal to {inches_value:.2f} inches.")
